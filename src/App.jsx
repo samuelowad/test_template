9ebad91349cadeb62b5b918e05d82fc04895076e
samuelowad <43174392+samuelowad@users.noreply.github.com>
2024-02-02 16:55:10 +0000

    adusted files

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "sam owad",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  gitHub: "",
+  instagram: "",
+  linkedIn: "",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
